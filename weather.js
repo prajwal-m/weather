622b038e873534ada7b7e915f8910a493a862504
prajwal-m <46987044+prajwal-m@users.noreply.github.com>
2019-09-24 18:48:46 +0530

    Add files via upload

--- weather.js
+++ weather.js
@@ -1,33 +1,64 @@
 const weather = (function() {
+  function displayData(weatherObj) {
+    $(".icon").addClass("display-block");
+    console.log(weatherObj);
+    const cityField = document.querySelector(".city"),
+      weatherField = document.querySelector(".weather-condition"),
+      temperatureField = document.querySelector(".temperature"),
+      humidityField = document.querySelector(".humidity"),
+      pressureField = document.querySelector(".pressure"),
+      windField = document.querySelector(".wind-speed");
 
-    function handleData(data) {
-        console.log(data);
-        console.log(data.weather[0].main);
-        var iconData =
-            "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-        console.log(iconData);
-        $(".icon").attr("src", iconData);
+    cityField.textContent = weatherObj.city + "-" + weatherObj.country;
+    weatherField.textContent = weatherObj.weatherCondition;
+    temperatureField.textContent = weatherObj.temperature;
+    humidityField.textContent = weatherObj.humidity;
+    pressureField.textContent = weatherObj.pressure;
+    windField.textContent = weatherObj.windSpeed;
+  }
 
-        console.log(data.main);
-        console.log(data.main.temp);
-    }
+  function handleData(data) {
+    const iconData =
+      "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    $(".icon").attr("src", iconData);
 
+    const weatherData = {
+      temperature: data.main.temp,
+      pressure: data.main.pressure,
+      humidity: data.main.humidity,
+      windSpeed: data.wind.speed,
+      weatherCondition: data.weather[0].main,
+      city: data.name,
+      country: data.sys.country
+    };
 
-    $("#submitLocation").click(function() {
-        //get value from input field
-        var city = $("#city").val();
+    displayData(weatherData);
+  }
 
-        $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/weather?q=" +
-                city +
-                "&units=metric" +
-                "&APPID=dd0a35b369a8e78de9c760a313ef7b00",
-            type: "GET",
-            dataType: "jsonp",
-            success: function(data) {
-                handleData(data);
-            }
-        });
+  function getData() {
+    var city = $("#city").val();
+
+    $.ajax({
+      url:
+        "http://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric" +
+        "&APPID=dd0a35b369a8e78de9c760a313ef7b00",
+      type: "GET",
+      dataType: "jsonp",
+      success: function(data) {
+        handleData(data);
+      }
     });
+  }
 
+  return {
+    getData: getData
+  };
+})();
+
+(function() {
+  document
+    .querySelector("#submitLocation")
+    .addEventListener("click", weather.getData);
 })();
